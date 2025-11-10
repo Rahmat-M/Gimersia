f0370220dafc622ec71556d1f5dca5608e067b87
Delvan Ramadhan <71202735+derudhan@users.noreply.github.com>
2025-11-10 16:36:02 +0700

    Rapihin folder Scripts
    Pindahin scripts yang berkaitan dengan mekanik Card ke foldernya sendiri buat backup.

--- Assets/_game/Scripts/CardMechanics/Card/CardInstance.cs
+++ Assets/_game/Scripts/CardMechanics/Card/CardInstance.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
-public class CardInstance
-{
+public class CardInstance {
     public CardData data;
     public CardInstance(CardData d) { data = d; }
 }
--- Assets/_game/Scripts/CardMechanics/GameManager.cs
+++ Assets/_game/Scripts/CardMechanics/GameManager.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     [Header("Systems")]
     public DeckSystem deck;
     public PlayerCombat playerCombat;
@@ -12,84 +11,66 @@ public class GameManager : MonoBehaviour
     //debug
     public CardData testReactionCard;
 
-    void Start()
-    {
+    void Start() {
         deck.OnCardPlayed += HandleCardPlayed;
     }
 
-    void HandleCardPlayed(CardInstance card)
-    {
+    void HandleCardPlayed(CardInstance card) {
         Debug.Log("[GameManager] Card played: " + card.data.cardName);
         // delegasikan ke PlayerCombat untuk mengeksekusi efek kartu
         playerCombat.PlayCard(card);
     }
 
     //debugging only
-    void Update()
-    {
+    void Update() {
         // Press 'H' untuk test add reaction card
-        if (Input.GetKeyDown(KeyCode.H))
-        {
+        if (Input.GetKeyDown(KeyCode.H)) {
             TestAddReactionCard(testReactionCard); // Assign di inspector
         }
 
         // Press 'J' untuk test take damage
-        if (Input.GetKeyDown(KeyCode.J))
-        {
+        if (Input.GetKeyDown(KeyCode.J)) {
             TestTakeDamage(20f);
         }
 
         // Press 'K' untuk test heal
-        if (Input.GetKeyDown(KeyCode.K))
-        {
+        if (Input.GetKeyDown(KeyCode.K)) {
             TestHeal(15f);
         }
     }
 
     // Method untuk testing: Add reaction card (bisa dipanggil dari UI button atau event)
-    public void TestAddReactionCard(CardData reactionCard)
-    {
-        if (reactionDeck != null)
-        {
+    public void TestAddReactionCard(CardData reactionCard) {
+        if (reactionDeck != null) {
             // Jika semua slot penuh, show replace panel
-            if (!reactionDeck.HasEmptySlot() && reactionDeck.GetReactionSlots().Count >= reactionDeck.maxReactionSlots)
-            {
-                var reactionUI = FindObjectOfType<ReactionDeckUI>();
-                if (reactionUI != null)
-                {
+            if (!reactionDeck.HasEmptySlot() && reactionDeck.GetReactionSlots().Count >= reactionDeck.maxReactionSlots) {
+                var reactionUI = FindFirstObjectByType<ReactionDeckUI>();
+                if (reactionUI != null) {
                     reactionUI.ShowReplaceCardPanel(reactionCard);
                 }
-            }
-            else
-            {
+            } else {
                 reactionDeck.AddReactionCard(reactionCard);
             }
         }
     }
 
     // Method untuk testing: Replace active card (untuk upgrade system)
-    public void TestReplaceActiveCard(CardData newActiveCard)
-    {
-        if (activeDeck != null)
-        {
+    public void TestReplaceActiveCard(CardData newActiveCard) {
+        if (activeDeck != null) {
             activeDeck.ReplaceActiveCard(newActiveCard);
         }
     }
 
     // Method untuk testing: Simulate damage (bisa dipanggil dari UI button)
-    public void TestTakeDamage(float damage)
-    {
-        if (playerHealth != null)
-        {
+    public void TestTakeDamage(float damage) {
+        if (playerHealth != null) {
             playerHealth.TakeDamage(damage);
         }
     }
 
     // Method untuk testing: Simulate heal (bisa dipanggil dari UI button)
-    public void TestHeal(float amount)
-    {
-        if (playerHealth != null)
-        {
+    public void TestHeal(float amount) {
+        if (playerHealth != null) {
             playerHealth.Heal(amount);
         }
     }
